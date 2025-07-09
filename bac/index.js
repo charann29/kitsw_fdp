b45823e4e90d03c1385efdf7b60405709af984f4
Gayatri23426 <ng.cse@kitsw.ac.in>
2025-07-09 12:23:40 +0530

    addaing node js

---
+++ bac/index.js
@@ -0,0 +1,19 @@
+let name = 'Node.js';
+const version = 20;
+
+// Function declaration
+//function greet(user) {
+//  return `Hello, ${user}!`; // Template literal (ES6)
+//}
+
+// Arrow function (ES6+)
+//const add = (a, b) => a + b;
+
+//console.log(greet('Gayatrir')); 
+//console.log(greet('Keerthi'));// Hello, Developer!
+//console.log(add(5, 3));// 8
+//console.log(add(10, 3));
+const greet = (user)=>{
+    return `Hello, ${user}`;
+    console.log(greet('Gayi'));
+}
